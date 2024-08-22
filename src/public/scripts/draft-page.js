de6a58db4057c1d3cb1187f768d516b75498c406
NoahK_MN <noah.kristensen@gmail.com>
2024-08-22 15:01:14 -0500

    better editing draft ui

--- src/public/scripts/draft-page.js
+++ src/public/scripts/draft-page.js
@@ -10,7 +10,9 @@ const resetPicksButton = document.querySelector('.reset-team-picks-btn');
 const resetTeamOrderButton = document.querySelector('.reset-team-order-btn');
 const saveDraftButton = document.querySelector('.save-draft-btn');
 const editDraftButton = document.querySelector('.edit-team-order-btn');
+
 renderPageHTML(); 
+
 function renderPageHTML(){
     loadPoiArrays();
     loadTeamArray();
@@ -24,8 +26,6 @@ function renderPageHTML(){
     } else{
         document.querySelector('.draft-message').innerText = 'Draft is Over';
     }
-    // document.querySelector('.index').innerHTML = indexOfDraft; implement this properly 
-    //where it says the team name as well as the current draft pick
 }
 
 function generateTeamHTML(){
@@ -47,7 +47,9 @@ function generateTeamHTML(){
         <div class="${isDrafting}">${firstPick}</div>
         <div class="${isDrafting}">${secondPick}</div>`
     });
+
     document.querySelector('.team-container').innerHTML = teamHTML; 
+    
     if (editDraftButton.classList.contains('is-editing')){
         generateTeamButtonEventListeners();
     }
@@ -66,6 +68,62 @@ function generatePoiHTML(){
     });
     document.querySelector('.we-pois-container').innerHTML = wePoiHTML;
     document.querySelector('.sp-pois-container').innerHTML = spPoiHTML; 
+    generateDraftButtonEventListeners();
+}
+
+undoButton.addEventListener('click', () => {
+    if (!attemptToUndoPrevSelection()){
+        return;
+    } 
+    saveTeams();
+    savePois();
+    renderPageHTML();
+});
+
+resetPicksButton.addEventListener('click', () => {
+    resetDraftPicks();
+    renderPageHTML();
+});
+
+resetTeamOrderButton.addEventListener('click', () => {
+    resetDraftOrder();
+    renderPageHTML();
+});
+
+saveDraftButton.addEventListener('click', () => {
+    if (indexOfDraft === 40){
+        saveDraftToDB();
+    }
+    else {
+        alert('You must finish a draft before you can save it');
+    }
+});
+
+editDraftButton.addEventListener('click', () => {
+    if (indexOfDraft !== 0){
+        alert(`You Can't edit the order of draft that has already started`);
+        return;
+    }
+    editDraftButton.classList.toggle('is-editing');
+    generateTeamHTML();
+});
+
+/**The event listener for the team button's consist of calling a swap function 
+ * This function will see if there has already been a team selected to swap with the team that was check selected
+ * if there exists a previously selected team then the function will swap the positions of the two teams
+ * Otherwise it will mark the team as ready to be swapped for when the next team is selected */
+function generateTeamButtonEventListeners(){
+    let teamButtons = document.querySelectorAll('.team-btn');
+        teamButtons.forEach(button =>{
+            button.addEventListener('click', ()=>{
+                if (attemptToSwapTeam(button)){
+                    generateTeamHTML();
+                }
+            });
+        });
+}
+
+function generateDraftButtonEventListeners(){
     let poiButtons = document.querySelectorAll('.poi-btn');
     poiButtons.forEach(button =>{
         button.addEventListener('click', () =>{
@@ -81,60 +139,6 @@ function generatePoiHTML(){
             saveTeams();
             savePois();
             renderPageHTML();
-        })
-    })
-}
-
-undoButton.addEventListener('click', () =>{
-    if(attemptToUndoPrevSelection()){ //only re-renders page if the undo was successful
-        saveTeams();
-        savePois();
-        renderPageHTML();
-    }
-});
-
-resetPicksButton.addEventListener('click', () =>{
-    resetDraftPicks();
-    renderPageHTML();
-});
-
-resetTeamOrderButton.addEventListener('click', () =>{
-    resetDraftOrder();
-    renderPageHTML();
-});
-
-saveDraftButton.addEventListener('click', ()=>{
-    if (indexOfDraft === 40){
-        saveDraftToDB();
-    }
-    else {
-        alert('You must finish a draft before you can save it');
-    }
-});
-
-editDraftButton.addEventListener('click', ()=>{
-    if (indexOfDraft !== 0){
-        alert(`You Can't edit the order of draft that has already started`);
-        return;
-    }
-    if (!editDraftButton.classList.contains('is-editing')){
-        editDraftButton.classList.add('is-editing');
-        generateTeamHTML();
-    }
-    else{
-        editDraftButton.classList.remove('is-editing');
-        generateTeamHTML();
-    }
-});
-
-function generateTeamButtonEventListeners(){
-    let teamButtons = document.querySelectorAll('.team-btn');
-        teamButtons.forEach(button =>{
-            button.addEventListener('click', ()=>{
-                console.log(button.innerText);
-                if (attemptToSwapTeam(button)){
-                    generateTeamHTML();
-                }
-            });
         });
+    });
 }
